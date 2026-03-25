6b762019a0c82013d94b9085626dfbab8d1bbb83
David Vargas <dvarrui@protonmail.com>
2026-03-25 14:18:32 +0000

    add more test add base 10

--- test/trusted-number_test.rb
+++ test/trusted-number_test.rb
@@ -3,12 +3,6 @@
 require "test_helper"
 
 class TrustedNumberTest < Test::Unit::TestCase
-  def setup
-    @bin1 = TrustedNumber.new("101.01", base: 2)
-    @bin2 = TrustedNumber.new("100.11", base: 2)
-    @bin3 = TrustedNumber.new("110.00", base: 2)
-  end
-
   test "VERSION" do
     assert do
       ::TrustedNumber.const_defined?(:VERSION)
@@ -16,10 +10,23 @@ class TrustedNumberTest < Test::Unit::TestCase
   end
 
   test "equal" do
-    assert(@bin1.value == "101.01")
+    bin1 = TrustedNumber.new("101.01", base: 2)
+    assert(bin1.value == "101.01")
   end
 
   test "add base 2" do
-    assert_equal(@bin3, @bin1 + @bin2)
+    bin1 = TrustedNumber.new("101.01", base: 2)
+    bin2 = TrustedNumber.new("100.11", base: 2)
+    bin3 = TrustedNumber.new("110.00", base: 2)
+
+    assert_equal(bin3, bin1 + bin2)
+  end
+
+  test "add base 10" do
+    dec1 = TrustedNumber.new("0.1")
+    dec2 = TrustedNumber.new("0.2")
+    dec3 = TrustedNumber.new("0.3")
+
+    assert_equal(dec3, dec1 + dec2)
   end
 end
