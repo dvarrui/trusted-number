bd8b3748707a2028ea9666a25cfd82681e144e1e
David Vargas <dvarrui@protonmail.com>
2026-04-01 09:28:54 +0100

    refactor

--- test/base10/equal_test.rb
+++ test/base10/equal_test.rb
@@ -10,17 +10,25 @@ class Base10EqualTest < Test::Unit::TestCase
   end
 
   test "new" do
-    assert(@zero.value == "0")
-    assert(@zero.base == 10)
+    assert_equal(@zero.value, "0")
+    assert_equal(@zero.base, 10)
+    assert_equal(@zero.predot, "0")
+    assert_equal(@zero.postdot, "0")
 
-    assert(@num1.value == "123.456")
-    assert(@num1.base == 10)
+    assert_equal(@num1.value, "123.456")
+    assert_equal(@num1.base, 10)
+    assert_equal(@num1.predot, "123")
+    assert_equal(@num1.postdot, "456")
 
-    assert(@num2.value == "123")
-    assert(@num2.base == 10)
+    assert_equal(@num2.value, "123")
+    assert_equal(@num2.base, 10)
+    assert_equal(@num2.predot, "123")
+    assert_equal(@num2.postdot, "0")
 
-    assert(@num3.value == "0.456")
-    assert(@num3.base == 10)
+    assert_equal(@num3.value, "0.456")
+    assert_equal(@num3.base, 10)
+    assert_equal(@num3.predot, "0")
+    assert_equal(@num3.postdot, "456")
   end
 
   test "equal" do
@@ -43,5 +51,14 @@ class Base10EqualTest < Test::Unit::TestCase
     assert(@num3 == TrustedNumber.new("0.456"))
     assert(@num3 == TrustedNumber.new("0.4560"))
     assert(@num3 == TrustedNumber.new(".4560"))
+  end
+
+  test "not equal" do
+    # refute_equal(@zero, TrustedNumber.new("0.1"))
+    # assert(@num1 != @num2)
+
+    # assert(TrustedNumber.new("0") != TrustedNumber.new("0", base: 2))
+    # assert(TrustedNumber.new("1") != TrustedNumber.new("1", base: 2))
+    # assert(TrustedNumber.new("10") != TrustedNumber.new("10", base: 2))
   end
 end
