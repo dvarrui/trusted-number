0fd029661d4464b1df787b2c304cbff20d956134
David Vargas <dvarrui@protonmail.com>
2026-04-01 10:57:55 +0100

    new example

---
+++ examples/new.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+require_relative "../lib/trusted-number"
+require "debug"
+
+def line
+  puts "\n" + "-" * 40
+end
+
+num = TrustedNumber.new("123.456")
+
+line
+puts "Example: 123.456"
+puts "- base    : #{num.base}"
+puts "- predot  : #{num.predot}"
+puts "- postdot : #{num.postdot}"
+puts "- value   : #{num.value}"
+puts "- about   : #{num.about}"
