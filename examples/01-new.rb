6f97741d44acb37ad97147b5360ad0c59b583b93
David Vargas <dvarrui@protonmail.com>
2026-07-26 20:12:36 +0100

    update example

--- examples/01-new.rb
+++ examples/01-new.rb
@@ -8,13 +8,14 @@ samples = [
   ["0101.00", 2, 0],
   [-1110, 2, -1],
   ["0ab.c0", 16, 0],
-  ["-0ab0.0e0", 16, 3]
+  ["-0ab0.0e0", 16, 3],
+  ["5,b", 10, 0],
 ]
 
 samples.each do |number, base, exp|
+  puts "==> Sample (#{number}, base: #{base}, exp: #{exp})"
   tnumber = TrustedNumber.new(number, base: base, exp: exp)
 
-  puts "==> Sample (#{number}, base: #{base}, exp: #{exp})"
-  puts "    #{tnumber.inspect}"
+  puts "    #{tnumber.inspect}" if tnumber.valid?
   puts ""
 end
